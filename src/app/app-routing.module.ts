3ac596beee87433f47c0fc826b741e52cb80b82e
AndresTobias <94875669+AndresTobias@users.noreply.github.com>
2022-01-25 10:48:04 -0300

    terminando enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
 const routes: Routes = [
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -22,4 +22,9 @@ export class HeroService {
   tomarDatosHeroes(): Hero[] {
     return HEROES;
   }
+  getHero(id: number): Observable<Hero> {
+  // TODO: send the message _after_ fetching the hero
+  this.messageService.add(`HeroService: fetched hero id=${id}`);
+  return of(HEROES.find(hero => hero.id === id));
+  }
 }
